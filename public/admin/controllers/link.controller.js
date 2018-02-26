a5b052609f9985a037b88007ffeb71492c27af65
Max Voskrekasenko <maks@nanocoding.com>
2018-02-26 18:06:51 +0200

    start

--- public/admin/controllers/link.controller.js
+++ public/admin/controllers/link.controller.js
@@ -2,12 +2,13 @@ class LinkCtrl {
   /* @ngInject */
   constructor($window, linkResource) {
     this._linkResource = linkResource;
-
     this.getCars();
   }
 
   getCars() {
-    return this._linkResource.query().$promise.then(res => console.log('res', res));
+    this._linkResource.query().$promise.then((res) => {
+      this.cars = res;
+    });
   }
 }
 
--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 import angular from 'angular';
 import uiRouter from 'angular-ui-router';
 import ngResource from 'angular-resource';
-import  LinkCtrl from './admin/controllers/link.controller';
+import LinkCtrl from './admin/controllers/link.controller';
 import linkResource from './admin/services/link.factory';
 import { theComponent } from './superAwesomeComponent/theComponent.js';
 import './admin/styles/main.sass';
@@ -25,20 +25,21 @@ angular.module('app', [uiRouter, ngResource])
   var managementKppState = {
     name: 'management-kpp',
     url: '/management-kpp',
-    template: '<h3>management of kpp page!</h3>'
+    template: '<h3>management of kpp page!</h3>',
   }
 
   var managementAutoState = {
     name: 'management-auto',
     url: '/management-auto',
-    template: '<h3>management of auto page!</h3>'
+    template: '<h3>management of auto page!</h3>',
   }
 
   var managementLinksState = {
     name: 'management-links',
     url: '/management-links',
     controller: 'LinkCtrl',
-    templateUrl: 'admin/templates/management-links.html'
+    controllerAs: '$ctrl',
+    templateUrl: 'admin/templates/management-links.html',
   }
 
   $stateProvider.state(managementKppState);
