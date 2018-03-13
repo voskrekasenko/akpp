d1249c0683c83bc3bc59da1f7d33ba42bbc24d5a
Max <voskrekasenko@gmail.com>
2018-03-13 22:17:48 +0200

    update and remove link

--- public/admin/controllers/link.controller.js
+++ public/admin/controllers/link.controller.js
@@ -5,7 +5,8 @@ class LinkCtrl {
     this._gearboxResource = gearboxResource;
     this._tableLinks = tableLinks;
     this.filter = new Filter('links');
-    this.dataLink = {};
+    this.fullLink = {};
+    this.fullLink.sendData = {};
     this.getCars();
     this.getGearboxes();
   }
@@ -22,33 +23,55 @@ class LinkCtrl {
   }
 
   addGearboxToInput(model) {
-    this.dataLink.gearboxModel = model.id;
+    this.fullLink.sendData.gearboxModel = model.id;
     this.filter.setParam('gearboxModel', model.id);
-    this._tableLinks.query(this.filter.filters.links).$promise.then((res) => {
-      this.links = res;
-      // console.log(res);
-    });
+    this.queryLinks();
   }
   addAutoYearToInput(year, modelId) {
-    this.dataLink.carModification = year.id;
-    this.dataLink.carModel = modelId;
+    this.fullLink.sendData.carModification = year.id;
+    this.fullLink.sendData.carModel = modelId;
     this.filter.setParam('carModification', year.id);
+    this.queryLinks();
+  }
+  queryLinks() {
     this._tableLinks.query(this.filter.filters.links).$promise.then((res) => {
       this.links = res;
-      // console.log(res);
     });
   }
-
   createLink(data) {
-    console.log(data);
     this._tableLinks.save(data).$promise.then((res) => {
-      console.log(res);
     });
   }
-
+  updateLink(data) {
+    this._tableLinks.update({ id: data.id }, data.sendData).$promise.then((res) => {
+      this.resetFullLink();
+      this.queryLinks();
+    });
+  }
+  removeLink(id) {
+    this._tableLinks.remove({ id }).$promise.then((res) => {
+      this.resetFullLink();
+      this.queryLinks();
+    });
+  }
+  getLink(data) {
+    this.fullLink.id = data.id;
+    this._tableLinks.get({ id: data.id }).$promise.then((res) => {
+      this.fullLink.sendData.gearboxModel = res.gearboxModel.id;
+      this.fullLink.sendData.carModification = res.carModification.id;
+      this.fullLink.sendData.carModel = res.carModel.id;
+      this.fullLink.sendData.driveUnit = res.driveUnit;
+      this.fullLink.sendData.engine = res.engine;
+    });
+  }
   resetFilters() {
     this.filter.resetParams();
     this.links = {};
+    this.resetFullLink();
+  }
+  resetFullLink() {
+    this.fullLink = {};
+    this.fullLink.sendData = {};
   }
 }
 
--- public/admin/services/table.links.resource.js
+++ public/admin/services/table.links.resource.js
@@ -1,3 +1,7 @@
 export default function tableLinks($resource) {
-  return $resource('/api/table-links');
+  return $resource('/api/table-links/:id', { id: '@_id' }, {
+    update: {
+      method: 'PUT',
+    },
+  });
 }
